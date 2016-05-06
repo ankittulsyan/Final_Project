9736a3874769f9fd8fd7cf4f15fcdefd91b43e13
Ankit Tulsyan <ankit.171185@gmail.com>
2016-05-06 18:39:16 +0800

    changes to the code

--- mainCode.m
+++ mainCode.m
@@ -1,4 +1,6 @@
 function mainCode()
-    %This will be the main code 
+    % This will be the main code 
     % This code will drive the entire code base
+    % Created by: ankit Tulsyan
+    % Date: 06/05/2016
 end
